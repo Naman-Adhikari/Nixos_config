15f6e5ff8bed99886979eecb6c257648a8741ef4
Naman-Adhikari <adhikarinaman00@gmail.com>
2026-01-19 21:11:41 +0545

    ghostty hot reload

--- home/scripts/riceswitch.sh
+++ home/scripts/riceswitch.sh
@@ -7,8 +7,12 @@ swww img "$WALL" --transition-type grow --transition-step 25 --transition-fps 15
 matugen image "$WALL"
 
 GHOSTTY_CONFIG="$HOME/.dotfiles/modules/ghostty/config";
+ROFI_CONFIG="$HOME/.dotfiles/modules/rofi/config.rasi";
 
 cp "$GHOSTTY_CONFIG" "$GHOSTTY_CONFIG.bak"
+cp "$ROFI_CONFIG" "$ROFI_CONFIG.bak"
 
 sed -i "s|^background-image = .*|background-image = \"$WALL\"|" "$GHOSTTY_CONFIG"
-
+sed -i "s|^background-image: .*|background-image: url(\"$WALL\", none);|" "$ROFI_CONFIG"
+sleep 0.8
+pkill -USR2 ghostty
