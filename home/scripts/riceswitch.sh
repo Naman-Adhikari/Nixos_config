f421e6875460adaeeea6d12271168b9a1018f4c8
Naman-Adhikari <adhikarinaman00@gmail.com>
2026-01-20 15:18:42 +0545

    tweak

--- home/scripts/riceswitch.sh
+++ home/scripts/riceswitch.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-WALL=$(find ~/.dotfiles/home/wallpapers -type f | shuf -n 1)
+
+WALL_DIR="$HOME/.dotfiles/home/wallpapers"
+LIST_FILE="$HOME/.cache/wallpaper_list"
+
+if [ ! -s "$LIST_FILE" ]; then
+    find "$WALL_DIR" -type f | shuf > "$LIST_FILE"
+fi
+
+WALL=$(head -n 1 "$LIST_FILE")
+
+sed -i '1d' "$LIST_FILE"
+
 
 swww img "$WALL" --transition-type grow --transition-step 25 --transition-fps 155
 
